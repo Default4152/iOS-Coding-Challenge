f34fe3a5abe4601f5a94747d6365752233a32562
Conner Alegre <cga0@pm.me>
2019-04-11 14:07:34 -0600

    Update AlbumTableViewController to include row height based on cell setup

--- iOS-Coding-Challenge/View Controllers/AlbumsTableViewController.swift
+++ iOS-Coding-Challenge/View Controllers/AlbumsTableViewController.swift
@@ -17,6 +17,7 @@ class AlbumsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.rowHeight = 90.0
         tableView.allowsSelection = false
         
         setupNavigationBar()
