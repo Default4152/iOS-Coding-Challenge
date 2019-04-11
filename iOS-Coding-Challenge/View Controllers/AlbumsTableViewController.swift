fb486c5387ab476d8d16efd97662479c8e29c45a
Conner Alegre <cga0@pm.me>
2019-04-11 13:42:09 -0600

    Add base TableViewCell

--- iOS-Coding-Challenge/View Controllers/AlbumsTableViewController.swift
+++ iOS-Coding-Challenge/View Controllers/AlbumsTableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class AlbumsTableViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
---
+++ iOS-Coding-Challenge/View/AlbumTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlbumTableViewCell.swift
+//  iOS-Coding-Challenge
+//
+//  Created by Conner on 4/11/19.
+//  Copyright © 2019 Conner. All rights reserved.
+//
+
+import UIKit
+
+class AlbumTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
