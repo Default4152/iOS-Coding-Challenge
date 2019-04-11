6681c4e11a938ae78f21a190d7a51d500e2056ed
Conner Alegre <cga0@pm.me>
2019-04-11 14:30:32 -0600

    Instantiates album controller and displays relevant data

--- iOS-Coding-Challenge/View Controllers/AlbumsTableViewController.swift
+++ iOS-Coding-Challenge/View Controllers/AlbumsTableViewController.swift
@@ -13,6 +13,7 @@ class AlbumsTableViewController: UITableViewController {
     // MARK: - Properties
     
     let reuseIdentifier = "AlbumCell"
+    var albumsController = AlbumsController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,17 +23,29 @@ class AlbumsTableViewController: UITableViewController {
         
         setupNavigationBar()
         
+        albumsController.getAlbums { (error) in
+            if let error = error {
+                print("Error with getting albums in TableView: \(error)")
+            }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+        
         tableView.register(AlbumTableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return albumsController.albums.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! AlbumTableViewCell
+        
+        let album = albumsController.albums[indexPath.row]
+        cell.album = album
         
         return cell
     }
